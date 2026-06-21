67188811286ae5ddcf7d6b0302516393a6e10e3e
Jéssica <jessicamendes6662@gmail.com>
2026-06-20 21:42:46 -0300

    bot1.0

---
+++ bot.py
@@ -0,0 +1,13 @@
+print("Olá, Mundo!")
+bot_name = "Cobrita"
+print(f"Meu nome é {bot_name}")
+adm_name = "Jéssica"
+print(f"O nome da minha criadora é {adm_name}")
+print(f"Este é o primeiro código em Python da {adm_name}")
+user_name = input("Qual é o seu nome?: ")
+print(f"Muito prazer em te conhecer, {user_name}!")
+user_age = int(input("E qual sua idade?: "))
+bot_age = 1
+print(f"Uau, vc tem {user_age} anos! Eu tenho apenas {bot_age} ano!")
+age_difference = user_age - bot_age
+print(f"Nossa diferença de idade é de {age_difference} anos!")
