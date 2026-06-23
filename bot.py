30bc593f043fd94ba906a2588db8dc103906bc81
Jéssica <jessicamendes6662@gmail.com>
2026-06-22 21:11:16 -0300

    Implement age verification in bot.py
    Added age verification logic and checks for user age.

--- bot.py
+++ bot.py
@@ -7,7 +7,14 @@ print(f"Este é o primeiro código em Python da {adm_name}")
 user_name = input("Qual é o seu nome?: ")
 print(f"Muito prazer em te conhecer, {user_name}!")
 user_age = int(input("E qual sua idade?: "))
+if user_age >= 18:
+  print("Você é maior de idade)
+else:
+  print("Você é menor de idade")
 bot_age = 1
 print(f"Uau, vc tem {user_age} anos! Eu tenho apenas {bot_age} ano!")
 age_difference = user_age - bot_age
 print(f"Nossa diferença de idade é de {age_difference} anos!")
+verification_age = input(f"{user_name}, vc é maior de idade?")
+if user_age = verification_age == 18: True
+else: False
